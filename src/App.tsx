b5dedbd820e5142a3693b41c881de1fdc065a742
Daniel Silva <dodosilva51@gmail.com>
2021-10-14 23:51:26 -0300

    Added first strucure
    Created .vscode and added configuration with specific details about the code and formatter
    
    Added packages needed like react, react submodules, styled-components and types for it
    
    Initialized npm configurations
    
    Initialized .git
    
    Added README blank
    
    Added tsconfig for ts files
    
    Added LICENSE ISC standards rituals
    
    Added .prettierrc config file for vscode to format properly the code patterns of the project
    
    Added .gitignore to ignore some knows
    
    Added first App and index.tsx with css to initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import './App.css';
+
+const AppContainer = styled.div`
+    font: 14px;
+`;
+
+function App() {
+    return <AppContainer>Hello World!</AppContainer>;
+}
+
+export default App;
